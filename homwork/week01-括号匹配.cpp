35a15f9479699793fe8d6a5faf17307eaaf3b0a7
luguanxing <651571925@qq.com>
2016-09-09 23:39:39 +0800

    Create week01-括号匹配.cpp

---
+++ homwork/week01-括号匹配.cpp
@@ -0,0 +1,70 @@
+/*
+Description
+检查输入字符串中的括号是否匹配。括号包括：{, }, (, ), [, ].
+
+Input
+第一行是一整数，即测试样例个数n.
+以下n行，每一行是一个长度不超过100个字符的字符串。
+
+Output
+第一行是一整数，即测试样例个数n.
+以下n行，每一行是一个长度不超过100的字符串。
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int cas;
+    cin >> cas;
+
+    while (cas--) {
+        string s, stack;
+        stack = " ";
+        cin >> s;
+        bool flag = true;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '{')
+                stack += '{';
+            else if (s[i] == '}') {
+                if (i == 0)
+                    flag = false;
+                if (stack[stack.size() - 1] == '{')
+                    stack.erase(stack.end() - 1);
+                else
+                    flag = false;
+            }
+
+            else if (s[i] == '(')
+                stack += '(';
+            else if (s[i] == ')') {
+                if (i == 0)
+                    flag = false;
+                if (stack[stack.size() - 1] == '(')
+                    stack.erase(stack.end() - 1);
+                else
+                    flag = false;
+            }
+
+            else if (s[i] == '[')
+                stack += '[';
+            else if (s[i] == ']') {
+                if (i == 0)
+                    flag = false;
+                if (stack[stack.size() - 1] == '[')
+                    stack.erase(stack.end() - 1);
+                else
+                    flag = false;
+            }
+
+        }
+        stack.erase(stack.begin());
+        if (flag && stack == "")
+            cout << "Yes" << endl;
+        else
+            cout << "No" << endl;
+    }
+    return 0;
+}
