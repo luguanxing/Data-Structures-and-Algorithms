b0eed6edf6b493fab6f7915d3cd2fd6f286ee39e
luguanxing <651571925@qq.com>
2017-08-08 00:35:48 +0800

    数组排序

---
+++ LeetCode/Java/561. Array Partition I.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Array_Partition_I {
+
+	public static void main(String[] args) {
+		int[] a = {1,4,3,2};
+		System.out.println(arrayPairSum(a));;
+	}
+	
+    public static int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int sum = 0;
+        for (int i = 0; i < nums.length; i+=2) {
+        		sum += nums[i];
+        }
+    	return sum;
+    }
+}
