fd47429a0e0eb25914c5dcdd3d677827c6f6d2e2
luguanxing <651571925@qq.com>
2017-08-10 01:25:39 +0800

    数出并设法移除重复的数

---
+++ LeetCode/Java/27. Remove Element.java
@@ -0,0 +1,36 @@
+import java.awt.List;
+import java.util.LinkedList;
+
+public class Remove_Element {
+
+	public static void main(String[] args) {
+		int[] a = {2, 2, 3};
+		System.out.println(removeElement(a, 2));
+		for (int i : a) {
+			System.out.print(i + " ");
+		}
+	}
+	
+    public static int removeElement(int[] nums, int val) {
+    	int ct = 0;
+    	for (int i = 0; i < nums.length; i++) {
+    		if (nums[i] != val)
+    			nums[ct++] = nums[i];
+    	}
+    	return ct;
+    }
+	
+//    public static int removeElement(int[] nums, int val) {
+//        int same = 0;
+//        LinkedList list = new LinkedList();
+//        for (int i = 0; i < nums.length; i++) {
+//        	if (nums[i] == val) 
+//        		same++;
+//        	else
+//        		list.add(nums[i]);
+//		}
+//        for (int i = 0; i < list.size(); i++)
+//        	nums[i] = (int) list.get(i);
+//    	return nums.length - same;
+//    }
+}
