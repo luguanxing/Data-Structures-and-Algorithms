1a0136a9f2d5d1061d23abb90e7983495f166d91
luguanxing <651571925@qq.com>
2017-08-11 00:30:19 +0800

    从数组移除重复数字

---
+++ LeetCode/Java/26. Remove Duplicates from Sorted Array.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Remove_Duplicates_from_Sorted_Array {
+
+	public static void main(String[] args) {
+		int[] a = {1};
+		System.out.println(removeDuplicates(a));
+	}
+	
+    public static int removeDuplicates(int[] nums) {
+    	if (nums.length == 0)
+    		return 0;
+    	int sum = 1;	//第一个肯定不是重复
+    	for (int i = 1; i < nums.length; i++) {
+    		if (nums[i] != nums[i-1])
+    			nums[sum++] = nums[i];
+    	}
+    	return sum;
+    }
+    
+//    public static int removeDuplicates(int[] nums) {
+//        Set set = new TreeSet();
+//        for (int i = 0; i < nums.length; i++)
+//        	set.add(nums[i]);
+//        int ct = 0;
+//        for (Object object : set) {
+//        	nums[ct++] = (int) object;
+//		}
+//    	return set.size();
+//    }
+    
+}
