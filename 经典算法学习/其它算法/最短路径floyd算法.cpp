a7ed11afd88c3599afb424757acc403193190a65
luguanxing <651571925@qq.com>
2017-12-03 00:25:33 +0800

    算法学习

---
+++ 经典算法学习/其它算法/最短路径floyd算法.cpp
@@ -0,0 +1,51 @@
+/*
+	Description
+	 给定一个无向图，用0,1,2,...,n-1代表其n个顶点，求指定两个顶点之间的最短距离。
+
+	Input
+	 
+
+	第一行为测试样例总数；
+
+	之后是N个测试样例，每个测试样例的第一行为4个数：顶点个数n，边数m, 顶点v1， 顶点v2。
+
+	n m v1 v2
+
+	之后是m行数对，每个数对<s,t>表示顶点s与顶点t之间存在边。
+
+	Output
+	对于每个样例，用单独一行输出最短距离。
+*/
+#include <iostream>
+#include <string.h>
+#include <algorithm>
+
+using namespace std;
+
+int a[105][105];
+int l[105][105];
+
+int main() {
+    int cas;
+    cin >> cas;
+    while (cas--) {
+        int n, m ,v1, v2, i, j, k;
+        for (i = 0; i < 105; i++) 
+            for (j = 0; j < 105; j++)
+                a[i][j] = l[i][j] = 999;
+        cin >> n >> m >> v1 >> v2;
+        for (i = 0; i < m; i++) {
+            int y, x;
+            cin >> y >> x;
+            a[y][x] = a[x][y] = 1;
+            l[y][x] = l[x][y] = 1;
+        }
+        for (k = 0; k < n; k++)
+            for (i = 0; i < n; i++) 
+                for (j = 0; j < n; j++)
+                    if (l[i][k] + l[k][j] < l[i][j])
+                         l[i][j] = l[i][k] + l[k][j];
+        cout << l[v1][v2] << endl;      
+    }
+    return 0;
+}                           
---
+++ 经典算法学习/其它算法/连连看BFS算法.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <queue>
+#include <stdlib.h>
+using namespace std;
+
+int chessdata[5][5] = {		//	此处棋盘为举例，实际时可修改
+	{6, 0, 0, 6, 1},
+	{2, 0, 0, 3, 1},
+	{3, 5, 0, 0, 0},
+	{1, 1, 0, 2, 5},
+	{6, 0, 0, 0, 1},
+};
+
+struct point {
+	int x, y, direct;	//	x, y为位置, direct为走的时候的方向
+	int step, hp;
+};
+point P, N, goal;
+point oper[4] = {{0, -1, 1}, {0, 1, 2}, {1, 0, 3}, {-1, 0, 4}};
+
+
+
+int llk_bfs(int y1, int x1, int y2, int x2) {	//	用BFS检测(y1,x1) 到 (y2,x2) 有没有转折2次以内的路径
+
+	bool cango[50][50], isvisit[50][50];
+	int t, row = 5, col = 5;	//	注意col为竖行数, row为横行数	
+
+	int temp1 = chessdata[y1][x1];
+	int temp2 = chessdata[y2][x2];
+	
+	chessdata[y1][x1] = 'S';
+	chessdata[y2][x2] = 'E';
+	
+	for (int i = 0; i < row; i++) {		//	对地图的标记
+		for (int j = 0; j < col; j++) {
+			isvisit[i][j] = false;
+			t = chessdata[i][j];
+			if (t == 0) {
+				cango[i][j] = true;
+			} else if (t == 'S') {
+				N.y = i;
+				N.x = j;
+				cango[i][j] = true;
+				isvisit[i][j] = true;
+			} else if (t == 'E') {
+				goal.y = i;
+				goal.x = j;
+				cango[i][j] = true;
+			} else {
+				cango[i][j] = false;
+			}
+		}
+	}
+
+	N.step = 0;
+	N.hp = 2;	//	转折最多为2次
+	N.direct = 0;
+
+
+	queue<point> Q;
+	Q.push(N);
+	while (!Q.empty()) {	//	BFS遍历S点到E点的所有路径
+		N = Q.front();
+		Q.pop();
+		if (N.x == goal.x && N.y == goal.y) {
+			chessdata[y1][x1] = temp1;
+			chessdata[y2][x2] = temp2;
+			return N.step;
+		}
+		for (int i = 0; i < 4; i++) {
+			P.x = N.x + oper[i].x;
+			P.y = N.y + oper[i].y;
+			P.step = N.step + 1;
+			P.direct = oper[i].direct;
+			P.hp = N.hp;
+			if (P.direct != N.direct && N.direct != 0)
+				P.hp -= 1;
+			if (P.x >= 0 && P.x <= col && P.y >= 0 && P.y <= row	//	对边界,重复和转折次数的限制
+					&& !isvisit[P.y][P.x] && cango[P.y][P.x] && P.hp >= 0) {
+				isvisit[P.y][P.x] = true;
+				Q.push(P);
+			}
+		}
+	}
+	chessdata[y1][x1] = temp1;
+	chessdata[y2][x2] = temp2;		
+	return -1;
+}
+
+int main() {
+	int y1, x1, y2, x2;
+	cin >> y1 >> x1 >> y2 >> x2;	//	输入要检测的两个位置
+ 	cout << llk_bfs(y1, x1, y2, x2) << endl;
+ 	return 0;
+}
