d79c51285267d0b17935198481c312288e0c0a25
luguanxing <651571925@qq.com>
2018-03-05 00:26:37 +0800

    JavaEE练习

---
+++ LeetCode/JavaEE/src/easy/Same_Tree.java
@@ -0,0 +1,26 @@
+package easy;
+
+public class Same_Tree {
+
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int x) { val = x; }
+	}
+	
+	class Solution {
+	    public boolean isSameTree(TreeNode p, TreeNode q) {
+	        if (p == null && q == null)
+	        	return true;
+	        else if  (p != null && q != null && p.val == q.val)
+	        	return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+	    	return false;
+	    }
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
