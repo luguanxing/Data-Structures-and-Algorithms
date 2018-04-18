c6b97b027d649a50f8170e678eefa78be2884604
luguanxing <651571925@qq.com>
2018-04-19 00:54:36 +0800

    正确使用synchronized实现生产消费者模式

--- 多核程序设计/Java/src/生产消费者/synchronized方式.java
+++ 多核程序设计/Java/src/生产消费者/synchronized方式.java
@@ -3,43 +3,46 @@ package 生产消费者;
 public class synchronized方式 {
 
 	public static Integer product = 0;
-	
+
+	// 锁对象，注意不能把product当成锁，否则修改导致不稳定，因用不变的对象做锁
+	public static Object lock = new Object();
+
 	public class Producer extends Thread {
 		@Override
 		public void run() {
 			while (true) {
-				synchronized (product) {
+				synchronized (lock) {
 					product++;
-					System.out.println("+++" + currentThread() + "已生产");
-					try {
-						sleep(1000);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
+					System.out.println("+++" + currentThread() + "已生产" + product);
+				}
+				try {
+					sleep(10);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
 				}
 			}
 		}
 	}
-	
+
 	public class Customer extends Thread {
 		@Override
 		public void run() {
 			while (true) {
-				synchronized (product) {
+				synchronized (lock) {
 					if (product > 0) {
 						product--;
-						System.out.println("---" + currentThread() + "已消费");
+						System.out.println("---" + currentThread() + "已消费" + product);
 					}
 				}
 			}
 		}
 	}
-	
+
 	public static void main(String[] args) {
-		new Thread(new synchronized方式().new Producer(), "生产者1号").start();
-		new Thread(new synchronized方式().new Customer(), "消费者1号").start();
-		new Thread(new synchronized方式().new Customer(), "消费者2号").start();
-		new Thread(new synchronized方式().new Customer(), "消费者3号").start();
+		for (int i = 1; i <= 3; i++)
+			new Thread(new synchronized方式().new Producer(), "生产者" + i + "号").start();
+		for (int i = 1; i <= 10; i++)
+			new Thread(new synchronized方式().new Customer(), "消费者" + i + "号").start();
 	}
 
 }
