2a881642d2f1700d13ad578f40f990be195ff3db
luguanxing <651571925@qq.com>
2018-05-11 01:06:12 +0800

    颠倒int的数位凑成新数

---
+++ LeetCode/JavaEE/src/easy/Reverse_Bits.java
@@ -0,0 +1,23 @@
+package easy;
+
+public class Reverse_Bits {
+
+	public class Solution {
+		public int reverseBits(int n) {
+			int result = 0;
+			for (int i = 0; i < 31; i++) {
+				result |= n & 1;
+				result <<= 1;
+				n >>= 1;
+			}
+			result |= n & 1;
+			return result;
+		}
+	}
+
+	public static void main(String[] args) {
+		int n = 43261596;
+		System.out.println(new Reverse_Bits().new Solution().reverseBits(n));
+	}
+
+}
