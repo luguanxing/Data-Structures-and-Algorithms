15a91f130828fb2feda9a019baa717ca3215d4eb
luguanxing <651571925@qq.com>
2018-05-11 01:17:41 +0800

    颠倒int的数位凑成新数

--- LeetCode/JavaEE/src/easy/Reverse_Bits.java
+++ LeetCode/JavaEE/src/easy/Reverse_Bits.java
@@ -4,6 +4,7 @@ public class Reverse_Bits {
 
 	public class Solution {
 		public int reverseBits(int n) {
+			// java本身不支持解析unsigned，所以不能用一般的方式解析n，只能用位运算移动达到翻转效果
 			int result = 0;
 			for (int i = 0; i < 31; i++) {
 				result |= n & 1;
