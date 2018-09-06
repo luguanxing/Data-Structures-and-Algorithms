3e79bf5bd909511bc1063d9aeeba52740f5d64ba
luguanxing <651571925@qq.com>
2018-09-06 19:42:50 +0800

    利用序号和数字循环轮换在O1找出数组中重复数字

--- LeetCode/JavaEE/src/medium/Find_the_Duplicate_Number.java
+++ LeetCode/JavaEE/src/medium/Find_the_Duplicate_Number.java
@@ -17,9 +17,29 @@ public class Find_the_Duplicate_Number {
 		}
 	}
 
+	class Solution_O1 {
+		public int findDuplicate(int[] nums) {
+			for (int i = 0; i < nums.length; i++) {
+				nums[i] -= 1;
+			}
+			for (int i = 0; i < nums.length; i++) {
+				while (nums[i] != i) {
+					if (nums[nums[i]] == nums[i]) {
+						return nums[i] + 1;
+					} else {
+						int temp = nums[nums[i]];
+						nums[nums[i]] = nums[i];
+						nums[i] = temp;
+					}
+				}
+			}
+			return -1;
+		}
+	}
+
 	public static void main(String[] args) {
 		int[] nums = { 1, 3, 4, 2, 2 };
-		System.out.println(new Find_the_Duplicate_Number().new Solution().findDuplicate(nums));
+		System.out.println(new Find_the_Duplicate_Number().new Solution_O1().findDuplicate(nums));
 	}
 
 }
